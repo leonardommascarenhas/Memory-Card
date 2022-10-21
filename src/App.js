d92dd375867e01d53c2bac93d5e26d96eaf762b5
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-10-21 12:19:11 -0300

    card creation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Card from "./components/Card/Card";
 import "./";
 
 const App = () => {
-  const [dogImage, setdogImage] = useState(null);
-  const [dogBreed, setDogBreed] = useState(null);
-  function apiRequest() {
-    fetch(`https://dog.ceo/api/breeds/image/random`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setdogImage(data.message);
-        let breed = data.message.split("/")[4];
-        console.log(breed);
-      });
-  }
-  useEffect(() => {
-    apiRequest();
-  }, []);
-
   return (
     <>
-      <button onClick={apiRequest}></button>
-      <div className="dog">
-        {dogImage && (
-          <img className="dogImage" src={dogImage} alt="doggo"></img>
-        )}
-      </div>
+      <Card />
     </>
   );
 };
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,1 +1,33 @@
-import React, { ùseState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import "./Card.css";
+
+const Card = () => {
+  const [dogImage, setdogImage] = useState(null);
+  const [dogBreed, setDogBreed] = useState(null);
+  function apiRequest() {
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+      .then((res) => res.json())
+      .then((data) => {
+        setdogImage(data.message);
+        let breed = data.message.split("/")[4];
+        setDogBreed(breed);
+      });
+  }
+  useEffect(() => {
+    apiRequest();
+  }, []);
+
+  return (
+    <>
+      <button onClick={apiRequest}></button>
+      <div className="dogCard">
+        {dogImage && (
+          <img className="dogImage" src={dogImage} alt="doggo"></img>
+        )}
+        <p>{dogBreed}</p>
+      </div>
+    </>
+  );
+};
+
+export default Card;
