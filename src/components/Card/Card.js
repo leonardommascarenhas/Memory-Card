1d8323322c362c42d0812baa730080b9a4d173cb
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-10-24 19:39:46 -0300

    yes baby

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,30 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./Card.css";
 
-const Card = () => {
-  const [dogImage, setdogImage] = useState(null);
-  const [dogBreed, setDogBreed] = useState(null);
-  function apiRequest() {
-    fetch(`https://dog.ceo/api/breeds/image/random`)
-      .then((res) => res.json())
-      .then((data) => {
-        setdogImage(data.message);
-        let breed = data.message.split("/")[4];
-        setDogBreed(breed);
-      });
-  }
-  useEffect(() => {
-    apiRequest();
-  }, []);
-
+const Card = ({ dogImage, dogBreed }) => {
   return (
     <>
-      <div className="dogCard">
-        {dogImage && (
-          <img className="dogImage" src={dogImage} alt="doggo"></img>
-        )}
-        <p>{dogBreed}</p>
-      </div>
+      <img className="dogImage" src={dogImage} alt="doggo"></img>
+      <p>{dogBreed}</p>
     </>
   );
 };
--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,30 +1,55 @@
 import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import "./GameBoard.css";
-
-let initialCards = [
-  <Card key={1} />,
-  <Card key={2} />,
-  <Card key={3} />,
-  <Card key={4} />,
-  <Card key={5} />,
-  <Card key={6} />,
-];
-
+import uniqid from "uniqid";
 const GameBoard = () => {
-  const [order, setOrder] = useState(initialCards);
+  let initialState = [];
+  const [dogObj, setDog] = useState([]);
+  const [order, setOrder] = useState([]);
+  function apiRequest() {
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+      .then((res) => res.json())
+      .then((data) => {
+        let breed = data.message.split("/")[4];
+        let obj = { BreedDog: breed, imgSrc: data.message, id: uniqid() };
+        setDog((current) => [...current, obj]);
+        setOrder((current) => [...current], obj);
+      });
+  }
+  useEffect(() => {
+    apiRequest(initialState);
+    apiRequest(initialState);
+    apiRequest(initialState);
+    apiRequest(initialState);
+    apiRequest(initialState);
+    apiRequest(initialState);
+    apiRequest(initialState);
+    apiRequest(initialState);
+    console.log(dogObj);
+  }, []);
 
   function handleClick() {
+    console.log(order);
     const nextOrder = [...order];
     nextOrder.sort(() => Math.random() - 0.5);
     setOrder(nextOrder);
   }
 
-  let card = document.querySelectorAll(".dogCard");
-  card.forEach((card) =>
-    card.addEventListener("click", () => console.log(card))
+  return (
+    <div className="gameBoard">
+      <button
+        onClick={() => {
+          apiRequest(initialState);
+          console.log(dogObj);
+        }}
+      ></button>
+      {dogObj.map((card) => (
+        <div key={card.id} onClick={handleClick}>
+          <Card dogImage={card.imgSrc} dogBreed={card.BreedDog} />
+        </div>
+      ))}
+    </div>
   );
-  return <div className="gameBoard">{order}</div>;
 };
 
 export default GameBoard;
