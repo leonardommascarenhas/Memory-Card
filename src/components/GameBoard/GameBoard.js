30a7dd739879ee9f92b372e5f8d95ddb70353164
leonardommascarenhas <leonardommascarenhas@gmail.com>
2022-10-27 22:00:07 -0300

    teste

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -5,6 +5,7 @@ import uniqid from "uniqid";
 import { AuthContext } from "../../providers/auth";
 
 const GameBoard = () => {
+  const [check, setCheck] = useState("");
   const [scored, setScored] = useState([]);
   const [order, setOrder] = useState([]);
   const { score, setScore } = React.useContext(AuthContext);
@@ -12,6 +13,7 @@ const GameBoard = () => {
   function apiRequest() {
     let count = 0;
     while (count < 8) {
+      console.log(count);
       count++;
       fetch(`https://dog.ceo/api/breeds/image/random`)
         .then((res) => res.json())
@@ -25,10 +27,11 @@ const GameBoard = () => {
   }
 
   useEffect(() => {
-    if (score % 8 == 0) {
+    if (score % 8 == 0 && score > 0) {
       console.log("win");
       setOrder([]);
       apiRequest();
+      setScore(0);
     }
   }, [score]);
 
@@ -36,8 +39,19 @@ const GameBoard = () => {
     apiRequest();
   }, []);
 
+  useEffect(() => {
+    if (scored.includes(check)) {
+      return console.log("fail");
+    } else {
+      console.log(scored);
+      setScored((current) => [...current, check]);
+      setScore((current) => current + 1);
+    }
+  }, [check]);
+
   function handleClick(e) {
-    setScore(score + 1);
+    let getDogBreed = e.target.src.split("/")[4];
+    setCheck(getDogBreed);
     const nextOrder = [...order];
     nextOrder.sort(() => Math.random() - 0.5);
     setOrder(nextOrder);
